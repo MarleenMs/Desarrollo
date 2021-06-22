c2c5f45b8e08f47fc84ab58d6584bdf9b6222d47
MarleenMs <marleen.munoz@comunidad.unam.mx>
2021-06-22 12:40:14 -0500

    HttpResponse

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'home.html', {})
+    return HttpResponse("Inicio")
